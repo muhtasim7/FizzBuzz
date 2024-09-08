5954e1a9ff55099962b77809a63e633e9e4da819
Muhtasim Khan <muhtasim.khan@mail.utoronto.ca>
2024-09-08 13:03:41 -0400

    changed the loop so that it is first 1000 numbers instead of 10

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,6 +1,6 @@
 public class Multiples {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 1000; i++) {
 
             if (i % 3 == 0 || i % 5 == 0) {
                 System.out.println(i);
